a19263ee55eca3feb12f7639673f01cac282b4fe
leanblesio <leanblesio@gmail.com>
2022-08-27 11:52:56 -0300

    itemlistcontainer

---
+++ src/components/itemListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const itemListContainer = ({greeting}) => {
+  return (
+    <div>{greeting}</div>
+  )
+}
+//agregado de codigo para push
+export default itemListContainer
